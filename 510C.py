dc8c1e30701246fd689614727ef5862a6ce37016
namantyagi2000 <naman18055@iiitd.ac.in>
2020-06-27 20:46:52 +0530

    File Added/Updated

---
+++ 510C.py
@@ -0,0 +1,130 @@
+# import sys
+# input = sys.stdin.readline
+from collections import deque
+
+class Graph(object):
+	"""docstring for Graph"""
+	def __init__(self,n,d): # Number of nodes and d is True if directed
+		self.n = n
+		self.graph = [[] for i in range(n)]
+		self.parent = [-1 for i in range(n)]
+		self.directed = d
+		
+	def addEdge(self,x,y):
+		self.graph[x].append(y)
+		if not self.directed:
+			self.graph[y].append(x)
+
+	def bfs(self, root): # NORMAL BFS
+		queue = [root]
+		queue = deque(queue)
+		vis = [0]*self.n
+		while len(queue)!=0:
+			element = queue.popleft()
+			vis[element] = 1
+			for i in self.graph[element]:
+				if vis[i]==0:
+					queue.append(i)
+					self.parent[i] = element
+					vis[i] = 1
+
+	def dfs(self, root, ans): # Iterative DFS
+		stack=[root]
+		vis=[0]*self.n
+		stack2=[]
+		while len(stack)!=0: # INITIAL TRAVERSAL
+			element = stack.pop()
+			if vis[element]:
+				continue
+			vis[element] = 1
+			stack2.append(element)
+			for i in self.graph[element]:
+				if vis[i]==0:
+					self.parent[i] = element
+					stack.append(i)
+
+		while len(stack2)!=0: # BACKTRACING. Modify the loop according to the question
+			element = stack2.pop()
+			m = 0
+			for i in self.graph[element]:
+				if i!=self.parent[element]:
+					m += ans[i]
+			ans[element] = m
+		return ans
+
+	def shortestpath(self, source, dest): # Calculate Shortest Path between two nodes
+		self.bfs(source)
+		path = [dest]
+		while self.parent[path[-1]]!=-1:
+			path.append(parent[path[-1]])
+		return path[::-1]
+
+	def detect_cycle(self):
+		indeg = [0]*self.n
+		for i in range(self.n):
+			for j in self.graph[i]:
+				indeg[j] += 1
+		q = deque()
+		vis = 0
+		for i in range(self.n):
+			if indeg[i]==0:
+				q.append(i)
+		ans = []
+		while len(q)!=0:
+			e = q.popleft()
+			vis += 1
+			ans.append(e)
+			for i in self.graph[e]:
+				indeg[i] -= 1
+				if indeg[i]==0:
+					q.append(i)
+		if vis!=self.n:
+			return True
+		return ans
+		return False
+
+
+	def reroot(self, root, ans):
+		stack = [root]
+		vis = [0]*n
+		while len(stack)!=0:
+			e = stack[-1]
+			if vis[e]:
+				stack.pop()
+				# Reverse_The_Change()
+				continue
+			vis[e] = 1
+			for i in graph[e]:
+				if not vis[e]:
+					stack.append(i)
+			if self.parent[e]==-1:
+				continue
+			# Change_The_Answers()
+
+n = int(input())
+g = Graph(26,True)
+done = {}
+s = input()
+start = s
+for i in range(n-1):
+	x = input()
+	j = 0
+	while j<min(len(s),len(x)) and s[j]==x[j]:
+		j += 1
+	if j==len(x) and len(s)>len(x):
+		print ("Impossible")
+		exit()
+	if j==len(s):
+		continue
+	if (ord(s[j]),ord(x[j])) not in done:
+		g.addEdge(ord(s[j])-97,ord(x[j])-97)
+		done[ord(s[j]),ord(x[j])] = 1
+	s = x
+order = g.detect_cycle()
+if order==True:
+	print ("Impossible")
+	exit()
+ans = ""
+for i in order:
+	ans += chr(i+97)
+print (ans)
